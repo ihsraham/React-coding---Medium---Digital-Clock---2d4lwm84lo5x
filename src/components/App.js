48c7eedd5582638e9a1f5a1dae07a5ba7a9f5dae
Maharshi Mishra <ihsraham27@gmail.com>
2021-12-11 19:24:45 +0530

    Made some changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
 
-  return (
-    <div id="main">
-      <div className="date-time"></div>
-    </div>
-  )
+function Clock() {
+  const [time, setTime] = useState(new Date());
+  function refreshTime() {
+    setTime(new Date());
+  }
+  useEffect(() => {
+    const id = setInterval(refreshTime, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+
+  return <div className="date-time">{time.toLocaleString}</div>;
 }
 
+const App = () => {
+  return (
+    <div id="main">
+      <Clock />
+    </div>
+  );
+};
 
 export default App;
